1cde03f225ee5508c7d1ae69ae9f840467978ec4
lapshuntanya <lapshun.tatiana@gmail.com>
2025-03-28 17:50:58 +0200

    set+get PaidLesson

--- PaidLesson.cpp
+++ PaidLesson.cpp
@@ -1,1 +1,38 @@
 #include "PaidLesson.h"
+#define DEBUG
+
+PaidLesson::PaidLesson()
+{
+    //Lesson() => C++, Inharitance, Create a base class
+    price = 0;
+#ifdef DEBUG
+    cout << "PaidLesson(): " << theme << endl;
+#endif // DEDUG
+}
+
+PaidLesson::PaidLesson(string theme, string info, string home, float price)
+    :Lesson(theme, info, home)
+{
+    //Lesson() => C++, Inheritance, Create a base class
+    //this->theme = theme;
+    this->price = price;
+#ifdef DEBUG
+    cout << "PaidLesson(t, i, h, p): " << theme << endl;
+#endif // DEDUG
+}
+
+PaidLesson::~PaidLesson()
+{
+#ifdef DEBUG
+    cout << "~PaidLesson(): " << theme << endl;
+#endif // DEDUG
+}
+
+void PaidLesson::setPrice(float price){ this->price = price; }
+float PaidLesson::getPrice() const{    return price;}
+
+void PaidLesson::showInfo() const
+{
+    Lesson::showInfo(); //theme, info, home
+    cout << "Price: " << price << "UAH.\n";
+}
